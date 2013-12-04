b4ccf1db2a02c4e1d5094576af02853453a1f07c
Simon Schmidt <schmidt.simon@gmail.com>
2013-12-04 23:31:59 +0100

    Wolfram Community

--- m_usage.meta.js
+++ m_usage.meta.js
@@ -4,9 +4,9 @@
 // @version       1.5
 // @updateURL     http://simonschmidt.github.io/SE-Usage-Message/m_usage.meta.js
 // @downloadURL   http://simonschmidt.github.io/SE-Usage-Message/m_usage.user.js
-// @description   ::usage tooltip for Mathematica symbols
+// @description	  ::usage tooltip for Mathematica symbols
 // @require       http://mutation-summary.googlecode.com/git/src/mutation-summary.js
 // @include       http://mathematica.stackexchange.com/*
 // @include       http://meta.mathematica.stackexchange.com/*
+// @include       http://community.wolfram.com/*
 // ==/UserScript==
-
